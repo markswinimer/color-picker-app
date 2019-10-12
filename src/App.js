1dcc751ae30e46e78e5b1cdc634a12d020a1b566
markswinimer <markswinimer@gmail.com>
2019-10-12 09:45:19 -0400

    adds slider library and implementation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     console.log(generatePalette(seedColors[4]));
     return (
       <div className="App">
-        <Palette {...seedColors[5]} />
+        <Palette palette={ generatePalette(seedColors[4]) } />
       </div>
     );
   }
