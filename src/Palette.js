fdf618e01ed40bd5df9ee7008211a80f210e7e7f
markswinimer <markswinimer@gmail.com>
2019-10-11 20:08:33 -0400

    created component, imported seeds

---
+++ src/Palette.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //
+    }
+  }
+  render() {
+    return(
+      <div className="Pallete">
+        {/* NAV */}
+        <div className="Palette-colors">
+          {/* COLOR BOXES */}
+        </div>
+        {/* FOOTER */}
+      </div>
+    )
+  }
+}
+
+export default Palette;
