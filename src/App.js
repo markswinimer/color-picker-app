8c8b8679de5b573eef280f632b0ddb27fbc45467
markswinimer <markswinimer@gmail.com>
2019-10-13 11:39:50 -0400

    imports and utilizes react router and creates root page with list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import Palette from "./Palette";
+import { Route, Switch } from 'react-router-dom';
 import seedColors from "./seedColors";
+import PaletteList from './PaletteList';
 import { generatePalette } from './colorHelpers'
 
 class App extends Component {
+  findPalette(id) {
+    // matches palette id to the palette from seedColors.js
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <Palette palette={ generatePalette(seedColors[4]) } />
-      </div>
-    );
+      <Switch>
+        <Route
+          exact
+          path="/" 
+          render={ () => <PaletteList palettes={seedColors}/> } />
+        <Route
+          exact
+          path="/palette/:id"
+          render={ routeProps => (
+            <Palette
+              palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+            />
+          )}
+        />
+        </Switch>
+      );
+    }
   }
-}
 
+  {/* <div className="App">
+  <Palette palette={ generatePalette(seedColors[4]) } />
+</div> */}
 export default App;
--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,17 +1,17 @@
 import chroma from 'chroma-js';
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-function generatePalette(staterPalette) {
+function generatePalette(starterPalette) {
   let newPalette = {
-    paletteName: staterPalette.paletteName,
-    id: staterPalette.id,
-    emoji: staterPalette.emoji,
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
     colors: {}
   };
   for(let level of levels) {
     newPalette.colors[level] = [];
   }
-  for(let color of staterPalette.colors) {
+  for(let color of starterPalette.colors) {
     let scale = getScale(color.color, 10).reverse();
     for(let i in scale) {
       newPalette.colors[levels[i]].push({
